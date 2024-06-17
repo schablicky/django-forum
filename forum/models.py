cd2f02d864403317a3d26b660f7e620034792a61
schablicky <98098735+schablicky@users.noreply.github.com>
2024-06-17 09:53:32 +0200

    Add files via upload

--- forum/models.py
+++ forum/models.py
@@ -2,8 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True, null=True)
+    name = models.CharField(max_length=100, verbose_name="Kategorie")
+    description = models.TextField(blank=True, null=True, verbose_name="Popisek")
 
     def __str__(self):
         return self.name
@@ -14,13 +14,13 @@ class Category(models.Model):
         verbose_name_plural = 'Kategorie'
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
-    image = models.ImageField(upload_to='media/', null=True, blank=True)
+    title = models.CharField(max_length=100, verbose_name="Nadpis")
+    content = models.TextField(max_length=155, verbose_name="Text")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Vytvořeno")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Upraveno")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Autor")
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True, verbose_name="Kategorie")
+    image = models.ImageField(upload_to='media/', null=True, blank=True, verbose_name="Obrázek")
 
     def __str__(self):
         return self.title
@@ -31,11 +31,11 @@ class Post(models.Model):
         verbose_name_plural = 'Příspěvky'
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE, verbose_name="Příspěvek")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Autor")
+    content = models.TextField(max_length=155, verbose_name="Text")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Vytvořeno")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Upraveno")
 
     def __str__(self):
         return f'Comment by {self.author} on {self.post}'
